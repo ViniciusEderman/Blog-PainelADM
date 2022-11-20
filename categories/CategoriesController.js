b6d6399732a772f3537046b75885bda4ec76c8a5
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-11-20 15:08:03 -0300

    nova rota e verificação de title como indefinido

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
-
-router.get("/categories", (req, res) => {
-    res.send("teste")
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("teste1")
+    res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    const title = req.body.title;
+
+    if(title != undefined) {
+        Category.create({
+
+            title: title,
+            slug: slugify(title),
+
+        }).then(() => {
+            res.redirect("/");
+        }).catch((err) => {
+            console.log(err);
+        })
+    }else {
+        res.redirect("/admin/categories/new");
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+    res.render("admin/categories/index");
 });
 
 module.exports = router;
