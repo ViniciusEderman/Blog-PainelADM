24ced1ffe793351135067e96d1d12d85d113eefe
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-11-22 12:04:28 -0300

    criando duas novas rotas

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -17,7 +17,7 @@ router.post("/categories/save", (req, res) => {
             slug: slugify(title),
 
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         }).catch((err) => {
             console.log(err);
         })
@@ -27,7 +27,51 @@ router.post("/categories/save", (req, res) => {
 });
 
 router.get("/admin/categories", (req, res) => {
-    res.render("admin/categories/index");
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories});
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+
+    const id = req.body.id;
+
+    if(id != undefined) {
+        if(!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id,
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        } 
+        else{
+            res.redirect("/admin/categories");
+        }
+    } 
+    else{
+        res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    const id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then((category) => { // pesquisa pelo id
+        if(category != undefined) {
+            res.render("admin/categories/edit", {category: category});
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(err => {
+        res.redirect("/admin/categories");
+        console.log(err);
+    });
 });
 
 module.exports = router;
