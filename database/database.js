19d4b2179a263a0ffa2b0d0b400b084644c457f8
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-11-29 11:53:57 -0300

    usando o timezone do brasil

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, { // utilizando o ENV para evitar vazar dados
     host: process.env.HOST,
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    timezone: "-03:00",
 });
 
 module.exports = connection;
