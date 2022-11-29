08f4a14a17485d9cf14b3395a6b3d13074850cc9
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-11-29 11:53:43 -0300

    criando a rota que salva os campos do front-end de articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
     res.send("teste")
 });
 
 router.get("/admin/articles/new", (req, res) => {
-   res.render("admin/articles/new");
+
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", 
+        {categories: categories});
+    })
+});
+
+router.post("/articles/save", (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title), // usando a biblioteca e gerando um slug em base do title
+        body: body,
+        categoryId: category,
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch((err) => {
+        console.log(err);
+        res.redirect("/");  
+    }); 
 });
 
 module.exports = router;
