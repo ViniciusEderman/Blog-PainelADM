5865d66678ee200a5f5ab2040d9b21068829cbe5
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-02-02 20:05:04 -0300

    utilizando hash password para segurança

--- users/UserController.js
+++ users/UserController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
-
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
 
@@ -21,7 +21,18 @@ router.post("/users/create", (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    res.json({ email, password });
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash,
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        console.log(err);
+        res.redirect("/");
+    });
 });
     
 module.exports = router;
