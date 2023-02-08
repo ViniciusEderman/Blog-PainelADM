2a37b9bd04396bd78cc24a5d38808515339d7b50
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-02-08 17:39:11 -0300

    mudando o redirecionamento

--- users/UserController.js
+++ users/UserController.js
@@ -69,7 +69,7 @@ router.post("/autenticacao", (req, res) => {
                     id: user.id,
                     email: user.email,
                 }
-                res.json(req.session.user);
+                res.redirect("/admin/articles");
             } else {
                 res.redirect("/login");
             }
