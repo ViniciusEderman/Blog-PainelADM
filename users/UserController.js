2c676746eb3fb0422cc3b0fa13480605602813cb
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-02-10 17:07:17 -0300

    finalizando o arquivo userController com a rota de logout

--- users/UserController.js
+++ users/UserController.js
@@ -79,5 +79,11 @@ router.post("/autenticacao", (req, res) => {
     });
 })
 
+router.get("/logout", (req, res) => {
+    
+    req.session.user = undefined;
+
+    res.redirect("/");
+});
     
 module.exports = router;
